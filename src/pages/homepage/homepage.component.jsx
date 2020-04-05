b6077930e63c124210ef2fd7deb1ead096d65ef9
Ashish Puttaa <ashishputtaa.98@gmail.com>
2020-04-05 13:50:29 +0530

    Initial Commit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import DirectoryMenu from './../../components/directory/directory.component';
+
+import './homepage.styles.scss';
+
+const HomePage = () => {
+   return (
+      <div className="homepage">
+         <DirectoryMenu />
+      </div>
+   );
+};
+
+export default HomePage;
