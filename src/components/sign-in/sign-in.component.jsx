5f0455de41f217e71e203b53b638e34e9e4079b9
Ashish Puttaa <ashishputtaa.98@gmail.com>
2020-04-09 19:59:10 +0530

    Implemented firebase utils, and included the ability to store authenticated users in firestore

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -32,7 +32,7 @@ class SignIn extends React.Component {
    render() {
       return (
          <div className="sign-in">
-            <h1 className="title">I already have an account</h1>
+            <h2 className="title">I already have an account</h2>
             <span>Sign in with your email and password</span>
 
             <form onSubmit={this.onSubmit}>
